18d1fbbe0e53a40e547bc1063f93e26d04ef8487
Nick <ncerda9586@gmail.com>
2020-09-03 06:29:45 -0500

    Completed all features and styling of website.

--- talentseeker-app/app/assets/scripts/app.js
+++ talentseeker-app/app/assets/scripts/app.js
@@ -1,8 +1,12 @@
 import '../styles/styles.css'
 import MobileMenu from './modules/MobileMenu'
+import RevealOnScroll from './modules/RevealOnScroll'
 
 
 let mobileMenu = new MobileMenu();
+
+new RevealOnScroll(document.querySelectorAll(".concentration__features"), 71)
+
 
 if (module.hot) {
     module.hot.accept()
